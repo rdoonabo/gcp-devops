6c91276bb67068c50e4867a1d1666fc4cd64d42d
ishaqmdgcp <55760349+ishaqmdgcp@users.noreply.github.com>
2021-05-10 19:22:06 +0530

    Update main.tf

--- local-exec/main.tf
+++ local-exec/main.tf
@@ -1,7 +1,7 @@
 # This is the provider used to spin up the gcloud instance
 provider "google" {
   credentials = file("terraform.json")
-  project = "migcpproject"
+  project = "nasa7733-lab"
   region  = "us-east1"
   
 }

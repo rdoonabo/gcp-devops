fcd07c1e305c3d510edd2b7ca156bef988537e31
ishaqmdgcp <55760349+ishaqmdgcp@users.noreply.github.com>
2021-08-11 00:24:31 +0530

    Update main.tf

--- VMInstance/main.tf
+++ VMInstance/main.tf
@@ -1,7 +1,7 @@
 # This is the provider used to spin up the gcloud instance
 provider "google" {
  
-  project = "nasa7733-lab"
+  project = "midevlab"
   credentials = file("terraform.json")
   region  = "us-east1"
   

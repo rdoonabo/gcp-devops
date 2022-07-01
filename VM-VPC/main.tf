351bf87da805f012604f168f7e584e2f690f7325
rdoonabo <88016140+rdoonabo@users.noreply.github.com>
2022-07-01 22:43:15 +0530

    Update main.tf

--- VM-VPC/main.tf
+++ VM-VPC/main.tf
@@ -4,9 +4,6 @@ provider "google" {
   region  = "europe-west2"
   zone    = "europe-west2-a"
 }
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
-}
 
 resource "google_compute_instance" "vm_instance" {
   name = "test-vm-${count.index}"
@@ -18,11 +15,19 @@ resource "google_compute_instance" "vm_instance" {
       image = "debian-cloud/debian-9"
     }
   }
-
-  network_interface {
-    network = google_compute_network.vpc_network.name
-    access_config {
-      // Ephemeral IP
+  resource "google_compute_subnetwork" "subnetwork" {
+  name          = "ravimi-subnetwork"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "europe-west2"
+  network       = google_compute_network.vpc_network.id
+ 
+}
+  resource "google_compute_network" "vpc_network" {
+  project                 = "ravi-mi"
+  name                    = "vpc-network"
+  auto_create_subnetworks = false
+  mtu                     = 1460
+}
     }
   }
 }

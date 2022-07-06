3826179f45db366f9072d3eb8541a4fa2f8d0418
rdoonabo <88016140+rdoonabo@users.noreply.github.com>
2022-07-06 09:35:51 +0530

    Update main.tf

--- VPC/main.tf
+++ VPC/main.tf
@@ -1,9 +1,9 @@
 provider "google" {
-  project = "nasa7733-lab"
-  region  = "europe-west2"
-  zone    = "europe-west2-a"
+  project = "ravi-mi"
+  region  = "us-central1"
+  zone    = "us-central1-c"
 }
 resource "google_compute_network" "vpc_network" {
-  name = "terraform-networks"
+  name = "kumar-networks"
 }
 

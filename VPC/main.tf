7b9f7ba99883b7078ae1bc3d6198ab6955912791
rdoonabo <88016140+rdoonabo@users.noreply.github.com>
2022-07-06 10:28:34 +0530

    Update main.tf

--- VPC/main.tf
+++ VPC/main.tf
@@ -3,8 +3,11 @@ provider "google" {
   region  = "us-central1"
   zone    = "us-central1-a"
 }
- resource "google_compute_network" "vpc_network" {
-    name = "ravik"
+resource "google_compute_network" "vpc_network" {
+  project                 = "ravi-mi"
+  name                    = "rrr"
+  auto_create_subnetworks = true
+  mtu                     = 1460
 }
 
 

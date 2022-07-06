156c25b339177735ce64ff045b3cd8c7086d7ecd
rdoonabo <88016140+rdoonabo@users.noreply.github.com>
2022-07-06 10:23:55 +0530

    Update main.tf

--- VPC/main.tf
+++ VPC/main.tf
@@ -3,26 +3,8 @@ provider "google" {
   region  = "us-central1"
   zone    = "us-central1-a"
 }
-// resource "google_compute_network" "vpc_network" {
-  // name = "drk-vpc"
-//}
-network_interface {
-     network  = google_compute_network.vpc_network.id
-     subnetwork = "ravi-mi-subnetwork"
-     access_config {
-      // Ephemeral IP
-     }
- }
- resource "google_compute_subnetwork" "subnetwork" {
-  name          = "ravi-mi-subnetwork"
-  ip_cidr_range = "10.3.0.0/16"
-  region        = "us-central1"
-  network  = google_compute_network.vpc_network.id
- 
-   }
-  resource "google_compute_network" "vpc_network" {
-  project                 = "ravi-mi"
-  name                    = "drk"
-  auto_create_subnetworks = false
-  mtu                     = 1460
-  }
+ resource "google_compute_network" "vpc_network" {
+    name = "ravik"
+}
+
+
